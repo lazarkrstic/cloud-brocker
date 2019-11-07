77c8b721143ac730d7af70367d90320e2d4b109e
lazarkrstic <lazarkrstic94@gmail.com>
2019-11-07 13:24:12 +0100

    sudo su

--- start_master.sh
+++ start_master.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo docker run -p 80:8080 -p 4566:4566 -p 873:873 --env MINPOOLSIZE=5 --env MAXPOOLSIZE=5 -it imilos/binder-master:v2
+sudo su
+docker run -p 80:8080 -p 4566:4566 -p 873:873 --env MINPOOLSIZE=5 --env MAXPOOLSIZE=5 -it imilos/binder-master:v2
