62efaf00cd7630a1ff9085b7444c3cce7c5c42c6
lazarkrstic <lazarkrstic94@gmail.com>
2019-11-15 01:35:13 +0100

    start docker service

--- start_worker.sh
+++ start_worker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sudo systemctl start docker
 sudo docker run --env BINDERHOST=wbmaster.cbp-routing.ch --env BINDERPORT=4566 --env BINDERMANAGERPORT=80 --env RSYNCPORT=873 -d imilos/binder-worker:v2
